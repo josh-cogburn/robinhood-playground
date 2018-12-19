017995be2c755f9601950931c6259359455998a4
John Murphy <chiefsmurph@gmail.com>
2018-12-19 13:35:33 -0800

    format settings

--- settings.js
+++ settings.js
@@ -5,44 +5,52 @@ module.exports = {
     purchaseAmt: 25,
     forPurchase: [
         // modern picks
-        '[postMongoBigHitters]',
-        '[postMongoTimes2]',
-        '[dec17]',
-        '[postMongoCount3]',
+            '[postMongoBigHitters]',
+            '[postMongoTimes2]',
+            '[dec17]',
+            '[postMongoCount3]',
 
         // old timers thanks to pm analysis
-        '[unprovenFilter30Drops]',
-        '[unprovenFilter30Drops]',
-        '[unprovenFilter30Drops]',
-        '[unprovenFilter30Drops]',
+            
+        // drops
+            '[filteredDrops]',
+            '[moreCommonDrops]',
+            '[unprovenSuddenDropsLast2Filter10]',
+            '[unprovenSuddenDropsLast2Filter10]',
 
-        '[unprovenFilter20Drops]',
-        '[unprovenFilter20Drops]',
+            '[unprovenFilter30Drops]',
+            '[unprovenFilter30Drops]',
+            '[unprovenFilter30Drops]',
+            '[unprovenFilter30Drops]',
 
-        '[sepAdds]',
-        '[sepHighlights]',
-        '[sepStars]',
-        '[hadAGoodDay]',
-        '[filteredDrops]',
-        '[moreCommonDrops]',
+            '[unprovenFilter20Drops]',
+            '[unprovenFilter20Drops]',
 
-        '[myRecs-day1-whileLoopCreme]',
-        '[myRecs-day7count3-hundredWithHundredCheck]',          /// ??????
-        '[myRecs-day10count5-hundredUpAvgGt4]',                 /// ??????
+        // september
+            '[sepAdds]',
+            '[sepHighlights]',
+            '[sepStars]',
+        
+        // more manuals
+            '[hadAGoodDay]',
+        
+        // my-recs
+            '[myRecs-day1-whileLoopCreme]',
+            '[myRecs-day7count3-hundredWithHundredCheck]',          /// ??????
+            '[myRecs-day10count5-hundredUpAvgGt4]',                 /// ??????
+            '[myRecs-day5count4-uniq-hundredUpPicks]',
+            '[myRecs-day3count2to4-slice16-uniq-hundredUpAvgGt4]',
 
+        // spm's
+            '[spm-52day-lowThirdMinCount5MagicScore-slice16-uniq]',
+            '[spm-52day-lowThirdMinCount5MagicScore-slice16-uniq]',
+            '[spm-52day-bestAvgTrendAnyPlayout]',
+            '[spm-52day-highLimitPlayoutsAvgTrend-slice16]',        /// ??????
+            '[spm-52day-bestFirstGreenAvgTrend-slice16-uniq]',      /// ??????
 
-        '[spm-52day-lowThirdMinCount5MagicScore-slice16-uniq]',
-        '[spm-52day-lowThirdMinCount5MagicScore-slice16-uniq]',
-        '[spm-52day-bestAvgTrendAnyPlayout]',
-        '[spm-52day-highLimitPlayoutsAvgTrend-slice16]',        /// ??????
-        '[spm-52day-bestFirstGreenAvgTrend-slice16-uniq]',      /// ??????
-        '[tiptop-minCount4]',
-        '[tiptop-minCount4]',
-
-        '[myRecs-day5count4-uniq-hundredUpPicks]',
-        '[myRecs-day3count2to4-slice16-uniq-hundredUpAvgGt4]',
-        '[unprovenSuddenDropsLast2Filter10]',
-        '[unprovenSuddenDropsLast2Filter10]',
+        // tiptop
+            '[tiptop-minCount4]',
+            '[tiptop-minCount4]',
     ],
     // forPurchaseVariation: '75Perc5Day-yesincludingblanks',
     fallbackSellStrategy: 'limit3',
