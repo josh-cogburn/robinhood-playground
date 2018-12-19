ea2418a8dadc9d6cc453ff56f3cd32b441c279fe
John Murphy <chiefsmurph@gmail.com>
2018-12-18 20:49:18 -0800

    going big or go home roll the dice ehh

--- settings.js
+++ settings.js
@@ -2,19 +2,33 @@
 module.exports = {
     // important settings
     sellAllStocksOnNthDay: 2,
-    purchaseAmt: 40,
+    purchaseAmt: 25,
     forPurchase: [
+        // modern picks
         '[postMongoBigHitters]',
-        '[postMongoBigHitters]',
-        '[postMongoBigHitters]',
-        '[postMongoBigHitters]',
-        
-        '[postMongoTimes2]',
         '[postMongoTimes2]',
         '[dec17]',
-        '[dec17]',
-
         '[postMongoCount3]',
+
+        // old timers thanks to pm analysis
+        '[unprovenFilter30Drops]',
+        '[unprovenFilter30Drops]',
+        '[unprovenFilter30Drops]',
+        '[unprovenFilter30Drops]',
+
+        '[sepAdds]',
+        '[sepHighlights]',
+        '[sepStars]',
+        '[hadAGoodDay]',
+        '[filteredDrops]',
+
+        '[spm-52day-lowThirdMinCount5MagicScore-slice16-uniq]',
+        '[spm-52day-lowThirdMinCount5MagicScore-slice16-uniq]',
+        '[spm-52day-bestAvgTrendAnyPlayout]',
+        '[myRecs-day5count4-uniq-hundredUpPicks]',
+        '[myRecs-day3count2to4-slice16-uniq-hundredUpAvgGt4]',
+        '[unprovenSuddenDropsLast2Filter10]',
+        '[unprovenSuddenDropsLast2Filter10]',
     ],
     fallbackSellStrategy: 'limit3',
     force: {
