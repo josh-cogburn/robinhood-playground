c86a5f656dde6c0e72d18aeac5bbec2efc645d67
John Murphy <chiefsmurph@gmail.com>
2018-12-29 14:46:28 -0800

    new ticker watchers

---
+++ modules/ticker-watchers/index.js
@@ -0,0 +1,9 @@
+module.exports = (Robinhood, trend) =>{
+    // find stocks to watch
+
+    // watch them, (register watch listeners)
+
+    // record picks when condition is met
+
+    // profit
+}
