3c9aa2b98b98557ef02be6553191498a8d6422e0
John Murphy <chiefsmurph@gmail.com>
2019-01-03 21:16:25 -0800

    fix

--- utils/fix-locale-date-string.js
+++ utils/fix-locale-date-string.js
@@ -1,9 +1,9 @@
 // fix because new server formats like - year-month-day
 // we want month-day-year
 const oldLocaleDateString = global.Date.toLocaleDateString;
-global.Date.prototype.toLocaleDateString = function() {
+Date.prototype.toLocaleDateString = function() {
     console.log('ouch baby.', this.getTime());
-    const prevOutput = toLocaleDateString.apply(this);
+    const prevOutput = oldLocaleDateString.apply(this);
     const [year, month, day] = prevOutput.split('-');
     return [month, day, year].join('-');
 };
