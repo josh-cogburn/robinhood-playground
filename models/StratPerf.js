6e8d8d4ec4eecb3c46bf51a147ffd4fcb0fa7eff
John Murphy <chiefsmurph@gmail.com>
2019-01-06 11:54:18 -0600

    cache StratPerfs for optimal recovery time

--- models/StratPerf.js
+++ models/StratPerf.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const cacheThis = require('../utils/cache-this');
 
 const schema = new Schema({
     date: { type: String, index: true },
@@ -13,25 +14,27 @@ const schema = new Schema({
 schema.statics.getUniqueDates = async function() {
     const response = await this.distinct('date');
     return response.sort((a, b) => new Date(a) - new Date(b));
-}
+};
 
-schema.statics.getByDate = async function(date) {
-    console.log('getting strat-perfs for day', date);
-    const recs = await this.find({ date });
-    const byBreakdown = {};
-    recs.forEach(rec => {
-        rec.perfs.forEach(({ period, avgTrend }) => {
-            byBreakdown[period] = (byBreakdown[period] || []).concat(
-                {
-                    strategyName: rec.stratMin,
-                    avgTrend,
-                }
-            );
+schema.statics.getByDate = cacheThis(
+    async date => {
+        console.log('getting strat-perfs for day', date);
+        const recs = await StratPerf.find({ date });
+        const byBreakdown = {};
+        recs.forEach(rec => {
+            rec.perfs.forEach(({ period, avgTrend }) => {
+                byBreakdown[period] = (byBreakdown[period] || []).concat(
+                    {
+                        strategyName: rec.stratMin,
+                        avgTrend,
+                    }
+                );
+            });
         });
-    });
 
-    return byBreakdown;
-};  
+        return byBreakdown;
+    }
+);
 
 const StratPerf = mongoose.model('StratPerf', schema, 'stratPerfs');
 module.exports = StratPerf;
