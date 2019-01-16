31bc3565e50dbddb747f5986c8939b73dd4587f2
John Murphy <chiefsmurph@gmail.com>
2019-01-16 07:59:29 -0800

    ey

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
 module.exports = {
     // important settings
     sellAllStocksOnNthDay: 2,
-    purchaseAmt: 11,
+    purchaseAmt: 25,
     forPurchase: [
         
         // st sent broken because rate limited ouch
