4b716f1dd08666928d9bb7f788824189e35f3e5a
John Murphy <chiefsmurph@gmail.com>
2019-01-16 06:48:21 -0800

    ah

--- rh-actions/already-bought-today.js
+++ rh-actions/already-bought-today.js
@@ -1,9 +1,9 @@
 const loadAllTransactionsSince = require('./load-all-transactions-since');
 
 module.exports = async (Robinhood, ticker) => {
-    
+    return false;
     const transactions = await loadAllTransactionsSince(Robinhood, 1);
-    // console.log(JSON.stringify(transactions, null, 2));
+    console.log(JSON.stringify(transactions, null, 2));
     return transactions.some(t => {
         return t.side === 'buy' && t.instrument.symbol === ticker;
     });
