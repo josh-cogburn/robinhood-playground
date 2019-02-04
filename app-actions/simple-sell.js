019e1fca93fe4ea694129d526218bf4716ce3ce3
John Murphy <chiefsmurph@gmail.com>
2019-02-04 00:39:25 -0700

    defend against accidental day trades

--- app-actions/simple-sell.js
+++ app-actions/simple-sell.js
@@ -2,6 +2,8 @@ const { force: { keep: keepers } } = require('../settings');
 
 const howMuchBoughtToday = require('./how-much-bought-today');
 const limitSellLastTrade = require('../rh-actions/limit-sell-last-trade');
+const alreadyBoughtToday = require('../rh-actions/already-bought-today');
+
 const jsonMgr = require('../utils/json-mgr');
 const lookup = require('../utils/lookup');
 
@@ -14,26 +16,22 @@ const addToDailyTransactions = async data => {
     await jsonMgr.save(fileName, curTransactions);
 };
 
-const calcQuantity = (maxPrice, bidPrice) => {
-    let quantity = Math.floor(maxPrice / bidPrice);
-    if (quantity === 0 && bidPrice < 50) {
-        quantity = 1;
-    }
-    return quantity;
-};
-
 module.exports = async (
     Robinhood,
     { ticker, quantity }
 ) => {
 
     if (keepers.includes(ticker)) {
-        throw 'ticker on keeper list';
+        throw 'ticker on keeper list: ' + ticker;
     }
 
     const boughtToday = await howMuchBoughtToday(Robinhood, ticker) || 0;
     if (boughtToday > 0) {
         throw 'already bought today: ' + ticker;
+    }
+
+    if (await alreadyBoughtToday(Robinhood, ticker)) {
+        throw 'not selling ' + ticker + 'because bought today'};
     }
 
     let { lastTrade, bidPrice: b } = await lookup(Robinhood, ticker);
--- rh-actions/already-bought-today.js
+++ rh-actions/already-bought-today.js
@@ -1,11 +1,24 @@
 const loadAllTransactionsSince = require('./load-all-transactions-since');
 
-module.exports = async (Robinhood, ticker) => {
-    return false;
-    const transactions = await loadAllTransactionsSince(Robinhood, 1);
-    console.log(JSON.stringify(transactions, null, 2));
-    return transactions.some(t => {
-        return t.side === 'buy' && t.instrument.symbol === ticker;
+const inDt = async (ticker) => {
+    const fileName = `./json/daily-transactions/${(new Date()).toLocaleDateString().split('/').join('-')}.json`;
+    const curTransactions = await jsonMgr.get(fileName) || [];
+    const foundInDT = curTransactions.some(transaction => {
+        return transaction.ticker === ticker && transaction.type === 'buy';
     });
+    return foundInDT;
+};
+
+module.exports = async (Robinhood, ticker) => {
+
+    const inTransactions = async () => {
+        const transactions = await loadAllTransactionsSince(Robinhood, 1);
+        console.log(JSON.stringify(transactions, null, 2));
+        return transactions.some(t => {
+            return t.side === 'buy' && t.instrument.symbol === ticker;
+        });
+    };
     
-}
+    return inDt(ticker) || await inTransactions();
+    
+};
