0c321339719c4be619b38f8e79ce81c8785bdde0
chiefsmurph <chiefsmurph@gmail.com>
2019-07-11 21:08:45 -0500

    target

--- settings.js
+++ settings.js
@@ -6,8 +6,8 @@ module.exports = {
     forPurchase: [
         '[kst-top100ZeroCrosses30minUnder300]',
         '[rsi-rhtopunder300]',
-        '[stocktwits-mostBearish]',
-        '[rsi-shouldWatchout]',
+        // '[stocktwits-mostBearish]',
+        // '[rsi-shouldWatchout]',
         '[rsi-lessthan15]',
     ],
     // forPurchaseVariation: '75Perc5Day-yesincludingblanks',
