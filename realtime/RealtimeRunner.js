a6133397c34825f5c9f5f2351dd98ae92ea2eea3
chiefsmurph <chiefsmurph@gmail.com>
2019-08-22 08:03:34 -0700

    fix

--- realtime/RealtimeRunner.js
+++ realtime/RealtimeRunner.js
@@ -130,7 +130,7 @@ module.exports = new (class RealtimeRunner {
         fn: () => this.stop()
     });
 
-    if (dayInProgress(START_MIN) && false) {
+    if (dayInProgress(START_MIN)) {
       console.log('in progress');
 
       const last5Minute = this.getLastTimestamp(5);
