67ccdc319012a34e9b8f75cb45eb96aac2f62669
chiefsmurph <chiefsmurph@gmail.com>
2019-09-24 08:57:51 -0500

    fix

--- tests/delete-junk-picks.js
+++ tests/delete-junk-picks.js
@@ -1,17 +1,17 @@
 const Pick = require('../models/Pick');
 
-const INCLUDE = ['rsi-'];
+const INCLUDE = ['lowVolumeTrash'];
 const DONT_INCLUDE = []//'rsi-', 'golden-cross-', 'sudden-drops-', 'pennyscan-'];
 
 
 
 module.exports = async () => {
   const todaysPicks = await Pick.find(
-      { date: '9-23-2019' },
+      { date: '9-24-2019' },
       { data: 0 }
   ).lean();
 
-
+  strlog({ todaysPicks: todaysPicks.length})
   const junk = todaysPicks.filter(({ strategyName }) => {
     return INCLUDE.every(str => strategyName.includes(str))
     // && DONT_INCLUDE.every(str => !strategyName.includes(str));
