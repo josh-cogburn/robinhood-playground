09e006ebaaa5508ef9972b20a7079c9fb373453d
chiefsmurph <chiefsmurph@gmail.com>
2019-09-30 08:55:21 -0500

    ah

--- modules/additional-cron.js
+++ modules/additional-cron.js
@@ -31,7 +31,7 @@ const stratManager = require('../socket-server/strat-manager');
 const additionalCron = [
     {
         name: 'alpacaSellAllStocks',
-        run: [0.4],
+        run: [30],
         fn: () => alpacaSellAllStocks()
     },
     // {
