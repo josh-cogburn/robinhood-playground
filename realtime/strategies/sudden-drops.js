f2cd6e42d8dc659ad7874b3056cd86f6a0f8189d
chiefsmurph <chiefsmurph@gmail.com>
2019-09-30 08:56:52 -0500

    ah

--- realtime/strategies/sudden-drops.js
+++ realtime/strategies/sudden-drops.js
@@ -63,6 +63,7 @@ module.exports = {
               },
               data: {
                 // allCurrents,
+                min,
                 mostRecent,
                 trendFromMin
               }
