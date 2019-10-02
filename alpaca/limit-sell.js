62879b8cdcbcb58693cf5190860026707849e82d
chiefsmurph <chiefsmurph@gmail.com>
2019-10-02 08:32:11 -0500

    fix

--- alpaca/limit-sell.js
+++ alpaca/limit-sell.js
@@ -38,7 +38,7 @@ const limitSell = async ({
     }
 
     await new Promise(resolve => setTimeout(resolve, 1000 * timeoutSeconds));
-    order = order ? await alpaca.getOrder(attemptResponse.id) : {};
+    order = order ? await alpaca.getOrder(order.id) : {};
 
     if (!order.filled_at) {
         await alpaca.cancelOrder(order.id);
