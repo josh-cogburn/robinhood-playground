fa1f0ba0c0f268f3f31740340400f094b49388ae
chiefsmurph <chiefsmurph@gmail.com>
2019-10-02 20:12:53 -0500

    or 1200

--- alpaca/get-balance.js
+++ alpaca/get-balance.js
@@ -3,5 +3,5 @@ const { alpaca } = require('.');
 module.exports = async _ => {
     const account = await alpaca.getAccount();
     console.log('Current Account:', account);
-    return Number(account.portfolio_value);
+    return Number(account.portfolio_value) || 1200;
 }
