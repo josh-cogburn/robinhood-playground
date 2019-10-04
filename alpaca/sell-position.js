fb13507d39dc59f0b6e1d95db67c093332e43e30
chiefsmurph <chiefsmurph@gmail.com>
2019-10-04 09:12:10 -0500

    ah!

--- alpaca/sell-position.js
+++ alpaca/sell-position.js
@@ -1,6 +1,7 @@
 const attemptSell = require('./attempt-sell');
 const Holds = require('../models/Holds');
 const sendEmail = require('../utils/send-email');
+const getTrend = require('../utils/get-trend');
 
 module.exports = async ({ ticker, quantity }) => {
 
