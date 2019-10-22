46883093d242ca5317a5576ee82a1ef41dcff649
chiefsmurph <chiefsmurph@gmail.com>
2019-10-21 22:11:55 -0700

    turnt up

--- settings.js
+++ settings.js
@@ -1,46 +1,55 @@
 // TODO: flatten list of strategies with PMs for emailObj same way as forPurchase
 
 let expectedPickCount = 0;
-const pm = (str, multiplier = 1, groupName) => {
+// const pm = (str, multiplier = 1, groupName) => {
 
-    let totalCount = 0;
-    const lines = str.split('\n').map(line => line.trim()).filter(Boolean);
-    const onlyPms = lines.map(line => {
-        const [pm, count] = line.split(' ');
-        totalCount += Number(count || 1);
-        return pm;
-    });
-    const withMultiplier = totalCount * multiplier;
-    console.log({
-        groupName,
-        totalCount,
-        withMultiplier
-    });
-    expectedPickCount += withMultiplier;
-    return Array(multiplier).fill(onlyPms).flatten().map(pm => `[${pm}]`);
-};
+//     let totalCount = 0;
+//     const lines = str.split('\n').map(line => line.trim()).filter(Boolean);
+//     const onlyPms = lines.map(line => {
+//         const [pm, count] = line.split(' ');
+//         totalCount += Number(count || 1);
+//         return pm;
+//     });
+//     const withMultiplier = totalCount * multiplier;
+//     console.log({
+//         groupName,
+//         totalCount,
+//         withMultiplier
+//     });
+//     expectedPickCount += withMultiplier;
+//     return Array(multiplier).fill(onlyPms).flatten().map(pm => `[${pm}]`);
+// };
 
 
 module.exports = {
     // important settings
     sellAllStocksOnNthDay: 8,
-    purchaseAmt: 7,
+    purchaseAmt: 2,
     forPurchase: [
 
-        // 2/5 sudden drops - TOTAL 42
 
+        // baselines
 
-        ...pm(`
-            sudden-drops-lunch 3
-            sudden-drops-lunch 3
-            sudden-drops-straightDown90 3
-            sudden-drops-straightDown90 3
-            sudden-drops-watchout-down
-        `, 1, 'simplicity'),  // 17
+        '[sudden-drops-lunch]',
+        '[sudden-drops-lunch]',
+        '[sudden-drops-majorJump]',
+        '[sudden-drops-majorJump-straightDown]',
 
-        ...pm(`
-            overnight-drops-!watchout-mediumJump 2
-        `, 1, 'overnight'),  // 17
+        // yes today trend high percUp
+        '[sudden-drops-lowVolFitty]',
+        '[sudden-drops-dinner-!down-!straightDown]',
+        '[sudden-drops-dinner-down15-straightDown]',
+        '[sudden-drops-!watchout-minorJump-!down-!straightDown]',
+        '[sudden-drops-mediumJump-dinner-down]',
+        '[sudden-drops-minorJump-down10]',
+        '[sudden-drops-minorJump-!straightDown]',
+
+        // no today trend but 90% percUp
+        '[sudden-drops-majorJump-down]',
+        '[sudden-drops-brunch-down10]',
+        '[sudden-drops-mediumJump-brunch-!straightDown]',
+        '[sudden-drops-!watchout-majorJump-!straightDown]',
+        '[sudden-drops-mediumJump-down15]',
 
     ],
     // forPurchaseVariation: '75Perc5Day-yesincludingblanks',
@@ -55,5 +64,5 @@ module.exports = {
             'MNGA'
         ]
     },
-    expectedPickCount
+    // expectedPickCount
 };
