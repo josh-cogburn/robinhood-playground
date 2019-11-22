3e7aae200aa6de51a1344a4e1740f501ee1118c4
chiefsmurph <chiefsmurph@gmail.com>
2019-11-22 09:26:12 -0700

    hey

--- settings.js
+++ settings.js
@@ -24,8 +24,8 @@ let expectedPickCount = 0;
 module.exports = {
     // important settings
     // sellAllStocksOnNthDay: 8,
-    disableMultipliers: false,
-    purchaseAmt: 30,
+    disableMultipliers: true,
+    purchaseAmt: 130,
     forPurchase: [
 
         ...`
