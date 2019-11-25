6f7bcd3ff2e85036bc7215c868069dae0855e7db
chiefsmurph <chiefsmurph@gmail.com>
2019-11-25 08:27:59 -0700

    turnt

--- settings.js
+++ settings.js
@@ -26,7 +26,7 @@ module.exports = {
     // sellAllStocksOnNthDay: 8,
     disableCashCheck: true,
     disableMultipliers: false,
-    purchaseAmt: 80,
+    purchaseAmt: 130,
     forPurchase: [
 
         ...`
