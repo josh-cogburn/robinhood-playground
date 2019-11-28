8b6149451e899e8f73df4c5400da6ba5792f995f
chiefsmurph <chiefsmurph@gmail.com>
2019-11-28 02:30:21 -0700

    ah

--- analysis/positions/get-closed.js
+++ analysis/positions/get-closed.js
@@ -5,7 +5,7 @@ module.exports = async () => {
   let closed = await ClosedPosition.find({}).lean();
   closed = await mapLimit(closed, 1, async position => ({
     ...position,
-    ...await analyzePosition(position);
+    ...await analyzePosition(position),
   }));
   return closed;
 };
