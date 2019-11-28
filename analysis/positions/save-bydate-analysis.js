e928543a493282c586b4e961c665fcc418a7e99d
chiefsmurph <chiefsmurph@gmail.com>
2019-11-27 20:22:49 -0700

    aight

--- analysis/positions/save-bydate-analysis.js
+++ analysis/positions/save-bydate-analysis.js
@@ -1,6 +1,6 @@
 const fs = require('mz/fs');
 const cTable = require('console.table');
-const { groupBy } = require('underscore');
+const { groupBy, mapObject } = require('underscore');
 
 const getAllPositions = require('./all');
 const analyzeGroup = require('./analyze-group');
@@ -40,18 +40,25 @@ module.exports = async () => {
   const lastFive = allDates.slice(0, 5);
   strlog({ allDates, lastFive })
   
-  const overall = {
-    allPositions: analyzeGroup(allPositions),
-    withoutKEG: analyzeGroup(allPositions.filter(({ ticker }) => ticker !== 'KEG')),
-    lastFive: analyzeGroup(allPositions.filter(({ date }) => lastFive.includes(date))),
-    yesterday: analyzeGroup(allPositions.filter(({ date }) => allDates[1] === date)),
-    today: analyzeGroup(allPositions.filter(({ date }) => allDates[0] === date)),
-    watchout: analyzeGroup(allPositions.filter(({ interestingWords }) => interestingWords.includes('watchout'))),
-    notWatchout: analyzeGroup(allPositions.filter(({ interestingWords }) => interestingWords.includes('!watchout'))),
-    bullish: analyzeGroup(allPositions.filter(({ interestingWords }) => interestingWords.includes('bullish'))),
-    neutral: analyzeGroup(allPositions.filter(({ interestingWords }) => interestingWords.includes('neutral'))),
-    bearish: analyzeGroup(allPositions.filter(({ interestingWords }) => interestingWords.includes('bearish'))),
-  };
+  const overall = mapObject({
+    allPositions: allPositions,
+    withoutKEG: allPositions.filter(({ ticker }) => ticker !== 'KEG'),
+    lastFive: allPositions.filter(({ date }) => lastFive.includes(date)),
+    yesterday: allPositions.filter(({ date }) => allDates[1] === date),
+    today: allPositions.filter(({ date }) => allDates[0] === date),
+    watchout: allPositions.filter(({ interestingWords }) => interestingWords.includes('watchout')),
+    notWatchout: allPositions.filter(({ interestingWords }) => interestingWords.includes('!watchout')),
+    bullish: allPositions.filter(({ interestingWords }) => interestingWords.includes('bullish')),
+    neutral: allPositions.filter(({ interestingWords }) => interestingWords.includes('neutral')),
+    bearish: allPositions.filter(({ interestingWords }) => interestingWords.includes('bearish')),
+    majorJump: allPositions.filter(({ interestingWords }) => interestingWords.includes('majorJump')),
+    mediumJump: allPositions.filter(({ interestingWords }) => interestingWords.includes('mediumJump')),
+    minorJump: allPositions.filter(({ interestingWords }) => interestingWords.includes('minorJump')),
+    singleMultiplier: allPositions.filter(({ numMultipliers }) => numMultipliers === 1),
+    multipleMultipliers: allPositions.filter(({ numMultipliers }) => numMultipliers > 1),
+    singlePick: allPositions.filter(({ numPicks }) => numPicks === 1),
+    multiplePicks: allPositions.filter(({ numPicks }) => numPicks > 1),
+  }, mapObject);
   
   await saveDateAnalysis(byDateAnalysis);
   await fs.writeFile('./json/overall-analysis.json', JSON.stringify(overall, null, 2));
