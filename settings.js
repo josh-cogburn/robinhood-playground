87f3f9c5e816243560444b1d8770d94427f15627
chiefsmurph <chiefsmurph@gmail.com>
2019-12-05 21:59:40 -0800

    turnt all the way down

--- settings.js
+++ settings.js
@@ -24,7 +24,7 @@ let expectedPickCount = 0;
 module.exports = {
     // important settings
     // sellAllStocksOnNthDay: 8,
-    purchaseAmt: 40,
+    purchaseAmt: 2,
     disableCashCheck: true,
     disableMultipliers: false,
     disableWatchouts: true,
