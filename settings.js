41d83d8ffe3a373540f5346aebf51a3107e8a66c
chiefsmurph <chiefsmurph@gmail.com>
2019-12-06 06:55:07 -0800

    okok

--- settings.js
+++ settings.js
@@ -24,7 +24,7 @@ let expectedPickCount = 0;
 module.exports = {
     // important settings
     // sellAllStocksOnNthDay: 8,
-    purchaseAmt: 2,
+    purchaseAmt: 20,
     disableCashCheck: true,
     disableMultipliers: false,
     disableWatchouts: true,
