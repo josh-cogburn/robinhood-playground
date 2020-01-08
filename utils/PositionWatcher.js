1929fdda503790ed4c2634c5e9596e22e5f008f2
chiefsmurph <chiefsmurph@gmail.com>
2020-01-08 07:38:31 -0800

    fix

--- utils/PositionWatcher.js
+++ utils/PositionWatcher.js
@@ -91,7 +91,7 @@ module.exports = class PositionWatcher {
           isBeforeClose
         },
         data: {
-          returnPerc
+          returnPerc,
           trendToLowestAvg,
         }
       }, true);
