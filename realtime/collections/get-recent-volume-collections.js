67d66655dbea3ad15c27a228eb598e890f8849d8
chiefsmurph <chiefsmurph@gmail.com>
2020-01-20 17:46:07 -0800

    another

--- realtime/collections/get-recent-volume-collections.js
+++ realtime/collections/get-recent-volume-collections.js
@@ -54,6 +54,12 @@ module.exports = async () => {
               .filter(result => result.recentVolume[prop])
               .filter(result => result.recentVolume.recentTrend > 0.2)
               .sort((a, b) => b.recentVolume[prop] - a.recentVolume[prop])
+              .slice(0, 7),
+            [`${key}UpUp`]: withRecentVolume
+              .filter(result => result.recentVolume[prop])
+              .filter(result => result.recentVolume.recentTrend > 0.2)
+              .filter(result => result.computed.tso && result.computed.tsc)
+              .sort((a, b) => b.recentVolume[prop] - a.recentVolume[prop])
               .slice(0, 7)
         };
     }, {});
