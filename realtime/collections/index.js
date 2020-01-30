4ad88d2e3114b0ab9406e74f93a6b29b923c4692
chiefsmurph <chiefsmurph@gmail.com>
2020-01-30 01:42:21 -0800

    game on

--- realtime/collections/index.js
+++ realtime/collections/index.js
@@ -3,18 +3,21 @@ const deriveCollections = require('./derive-collections');
 const { mapObject } = require('underscore');
 
 module.exports = async dontMerge => {
+  console.log('getting base collections....')
   const baseCollections = await getBaseCollections();
+  console.log('getting derived collections....')
   const derivedCollections = await deriveCollections(baseCollections);
   const merged = {
     ...baseCollections,
     ...derivedCollections
   };
-  strlog(
-    mapObject(
-      merged,
-      results => results.length
-    )
-  );
+  // strlog(
+  //   mapObject(
+  //     merged,
+  //     results => results.length
+  //   )
+  // );
+  console.log('leaving collections index')
   return dontMerge ? {
     baseCollections,
     derivedCollections
