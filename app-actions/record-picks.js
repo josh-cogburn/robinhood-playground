69a49eba3b520c632eaa8d0dfa5c5f93fa47b16c
chiefsmurph <chiefsmurph@gmail.com>
2020-02-18 19:03:55 -0800

    woops

--- app-actions/record-picks.js
+++ app-actions/record-picks.js
@@ -77,9 +77,9 @@ const saveToFile = async (strategy, min, withPrices, { keys, data }) => {
 
         const onlyMinor = interestingWords.includes('minorJump') && !interestingWords.includes('mediumJump') || !interestingWords.includes('majorJump');
         if (onlyMinor) {
-            multiplier = Math.max(3, multiplier);   // min of 3
+            multiplier = Math.max(2, multiplier);   // min of 3
             if (!interestingWords.includes('downer')) {
-                multiplier = Math.min(10, multiplier);  // max of 10 ... if not avg downer
+                multiplier = Math.min(8, multiplier);  // max of 10 ... if not avg downer
             }
         }
         
