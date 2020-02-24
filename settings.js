7564a59c7dafd4040db16a782fc64aa5e00cdd6d
chiefsmurph <chiefsmurph@gmail.com>
2020-02-24 09:50:15 -0500

    1

--- settings.js
+++ settings.js
@@ -42,7 +42,7 @@ module.exports = {
     },
     
     // sellAllStocksOnNthDay: 8,
-    purchaseAmt: 2,
+    purchaseAmt: 1,
     makeKeeperFundsAvailable: false,
     // expectedPickCount: 200,
     multiplierThreshold: 4, // wont recommend picks below this multiplier count even if they are a forPurchase pick
