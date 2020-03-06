ab5225deb5f107f4267cb194e1dbcf26b8088a44
chiefsmurph <chiefsmurph@gmail.com>
2020-03-06 10:06:47 -0400

    yep

--- socket-server/strat-manager.js
+++ socket-server/strat-manager.js
@@ -103,7 +103,7 @@ const stratManager = {
         console.log('initd strat manager');
     },
     resetPositionWatchers() {
-        for (let pos of this.positions.alpaca) {
+        for (let pos of this.positions.alpaca.sort((b.equity - a.equity))) {
             if (pos.daysOld <= settings.continueDownForDays) {
                 console.log(`starting avg downer ${pos.ticker} bc less than continueDownForDays days old`)
                 watchThis({
